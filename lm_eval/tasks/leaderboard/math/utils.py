eea526cd67d770e865eb10218eaa75a327f32c4d
RicardoDominguez <riddoddo@gmail.com>
2024-07-02 19:28:05 +0200

    math strip 3

--- lm_eval/tasks/leaderboard/math/utils.py
+++ lm_eval/tasks/leaderboard/math/utils.py
@@ -68,7 +68,7 @@ def process_results(doc: dict, results: List[str]) -> Dict[str, int]:
     candidates = results[0]
 
     unnormalized_answer = get_unnormalized_answer(candidates)
-    answer = normalize_final_answer(unnormalized_answer)
+    answer = normalize_final_answer(unnormalized_answer).strip()
 
     if is_equiv(answer, doc["answer"]):
         retval = 1
