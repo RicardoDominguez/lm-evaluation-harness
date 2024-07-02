0f6af8e17033f6c5db5a3394a6409d83329e50c8
RicardoDominguez <riddoddo@gmail.com>
2024-07-02 20:03:05 +0200

    verbose

--- lm_eval/tasks/leaderboard/math/utils.py
+++ lm_eval/tasks/leaderboard/math/utils.py
@@ -69,7 +69,12 @@ def process_results(doc: dict, results: List[str]) -> Dict[str, int]:
 
     unnormalized_answer = get_unnormalized_answer(candidates)
     answer = normalize_final_answer(unnormalized_answer)
-    answer = answer.strip()
+
+    # print everything
+    print('candidates:', candidates)
+    print('unnormalized_answer:', unnormalized_answer)
+    print('answer:', answer)
+    print('doc["answer"]:', doc["answer"])
 
     if is_equiv(answer, doc["answer"]):
         retval = 1
